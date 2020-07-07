f3eb619e8d26ed8cf2b54cc3b2299fc5c68a3858
Chuck Junior <59852540+ceejay-el@users.noreply.github.com>
2020-07-07 12:49:56 -0700

    done

---
+++ RockPaperScissors.py
@@ -0,0 +1,34 @@
+# Rock, Paper Scissors
+
+import random
+
+def rockPaperScissors():
+    programSelect = (random.choice(["rock", "paper", "scissors"]))
+    playerChoice = input("Rock, paper, scissors? ").lower()
+
+    if programSelect == playerChoice:
+        restart = input("Draw, play again? [y or n]").lower()
+        playAgain(restart)
+    elif (programSelect == "rock" and playerChoice == "scissors") or (programSelect == "scissors" and playerChoice == "paper") or (programSelect == "paper" and playerChoice == "rock"):
+        restart = input("You lose! Play again? [y or n]").lower()
+        playAgain(restart)
+    elif (playerChoice == "rock" and programSelect == "scissors") or (playerChoice == "scissors" and programSelect == "paper") or (playerChoice == "paper" and programSelect == "rock"):
+        restart = input("You win! Play again? [y or n]").lower()
+        playAgain(restart)
+    else:
+        restart = input("Computer *facepalms: Play again? [y or n]").lower()
+        playAgain(restart)
+
+
+def playAgain(restart):
+    if restart == "y":
+        rockPaperScissors()
+    elif restart != "y" and restart != "n":
+        print("Oy-vey")
+    else:
+        exit()
+
+#main
+gameRunning = True
+while gameRunning:
+    rockPaperScissors()
